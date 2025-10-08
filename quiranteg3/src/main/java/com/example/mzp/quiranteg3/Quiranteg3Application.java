e688c5b2ce40847d10dc34698e05e9296e53fadd
zilfaedz <zilfaedz.quirante@cit.edu>
2025-10-08 16:07:35 +0800

    first commit

---
+++ quiranteg3/src/main/java/com/example/mzp/quiranteg3/Quiranteg3Application.java
@@ -0,0 +1,13 @@
+package com.example.mzp.quiranteg3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quiranteg3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Quiranteg3Application.class, args);
+	}
+
+}
---
+++ quiranteg3/src/test/java/com/example/mzp/quiranteg3/Quiranteg3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mzp.quiranteg3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Quiranteg3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

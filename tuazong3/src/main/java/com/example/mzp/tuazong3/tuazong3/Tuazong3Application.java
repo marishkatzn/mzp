5dd94996e263ab0f4491f3f18b1150494eed2489
Marishka Cristienne T. Tuazon <marishkacristienne.tuazon@cit.edu>
2025-10-22 13:31:55 +0800

    initial commit

---
+++ tuazong3/src/main/java/com/example/mzp/tuazong3/tuazong3/Tuazong3Application.java
@@ -0,0 +1,13 @@
+package com.example.mzp.tuazong3.tuazong3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tuazong3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tuazong3Application.class, args);
+	}
+
+}
